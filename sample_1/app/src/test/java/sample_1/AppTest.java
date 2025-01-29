9deec2959fdb802d86ae5581f5594bcad6b5733d
JacksonFurrier <szaqaei@inf.elte.hu>
2025-01-29 19:41:49 +0100

    More changes to work with Junit5.

--- sample_1/app/src/test/java/sample_1/AppTest.java
+++ sample_1/app/src/test/java/sample_1/AppTest.java
@@ -3,12 +3,15 @@
  */
 package sample_1;
 
+import sample_1.App;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.TestInstance;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AppTest {
 
     App mApp;
