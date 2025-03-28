b868cba36882fc0809f6360c643f41a2f1c5e2ea
korpap <34884631+IModer@users.noreply.github.com>
2025-03-28 20:55:15 +0100

    eep

--- assignment/app/src/test/java/org/example/WalrusTest.java
+++ assignment/app/src/test/java/org/example/WalrusTest.java
@@ -4,9 +4,22 @@
 package org.example;
 
 import org.junit.Test;
+import org.junit.Test.Before;
 import static org.junit.Assert.*;
 
 public class WalrusTest {
     @Test public void appHasAGreeting() {
     }
+
+    //Setup function before all tests
+    @Before
+    public void setup() {
+        
+    }
+
+    // 1. Write a test to see how much a Walrus can eat 
+    // 2. Write a test to check if a Walrus gets the right food 
+    // 3. Write a test to check opening a can will return food 
+    // 4. Write a test to check on how a Walrus can eat
+    // 5. Write a test making a Walrus accept non-Walrus food 
 }
