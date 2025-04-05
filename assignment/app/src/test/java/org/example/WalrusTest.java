6444ccce604906b2a1358ee1cfda4174fa3a8d10
Mihalkó Norbert <mihalko.norbi@gmail.com>
2025-04-05 20:20:07 +0200

    This criterion is linked to a Learning OutcomeWrite a test to see how much a Walrus can eat

--- assignment/app/src/test/java/org/example/WalrusTest.java
+++ assignment/app/src/test/java/org/example/WalrusTest.java
@@ -6,7 +6,34 @@ package org.example;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import org.example.values.Walrus;
+import org.example.values.WalrusFood;
+import org.junit.*;;
+
 public class WalrusTest {
-    @Test public void appHasAGreeting() {
+    private Walrus walrus;
+
+    @Before
+    public void setUp() {
+        walrus = new Walrus();
+    }
+
+    @Test
+    public void testHowMuchWalrusCanEat() {
+        int i;
+        for (i = 0; i < 10000; i++) {
+            try {
+                walrus.addToStomach(new WalrusFood());
+            } catch (Exception e) {
+                break;
+            }
+        }
+
+        if(i < 10000)
+            System.out.println(String.format("Walrus can eat %04d foods.", i));
+        else
+            System.out.println("Walrus can ate all our foods.");
+
+        assertTrue(true);
     }
 }
