640e00a36bf492d1608b47b69e868f4cebb81cb7
Remarci225 <71138785+Remarci225@users.noreply.github.com>
2025-04-14 03:08:09 +0200

    Implement Walrus tests

--- assignment/app/src/test/java/org/example/WalrusTest.java
+++ assignment/app/src/test/java/org/example/WalrusTest.java
@@ -3,10 +3,63 @@
  */
 package org.example;
 
+import org.example.values.CannedWalrusFood;
+import org.example.values.Walrus;
+import org.example.values.WalrusFood;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class WalrusTest {
-    @Test public void appHasAGreeting() {
+    @Test
+    public void testHowMuchAWalrusCanEat() {
+        Walrus gary = new Walrus();
+
+        for (int i = 0; i < 5; ++i) {
+            WalrusFood walrusFood = new WalrusFood();
+            gary.addToStomach(walrusFood);
+            assertTrue(gary.hasEaten(walrusFood));
+        }
+    }
+
+    @Test
+    public void testIfAWalrusGetsTheRightFood() {
+        Walrus gary = new Walrus();
+        WalrusFood walrusFood = new WalrusFood();
+        CannedWalrusFood cannedWalrusFood = new CannedWalrusFood(walrusFood);
+        OpensCan opensCan = new OpensCan();
+
+        assertEquals(walrusFood, opensCan.open(cannedWalrusFood));
+    }
+
+    @Test
+    public void testIfOpeningACanWillReturnFood() {
+        WalrusFood walrusFood = new WalrusFood();
+        CannedWalrusFood cannedWalrusFood = new CannedWalrusFood(walrusFood);
+        OpensCan opensCan = new OpensCan();
+
+        assertNotNull(opensCan.open(cannedWalrusFood));
+    }
+
+    @Test
+    public void testHowAWalrusCanEat() {
+        Walrus gary = new Walrus();
+        WalrusFood walrusFood = new WalrusFood();
+        CannedWalrusFood cannedWalrusFood = new CannedWalrusFood(walrusFood);
+
+        FeedsWalrus feedsWalrus = new FeedsWalrus();
+        feedsWalrus.feed(gary, cannedWalrusFood);
+
+        assertNull(cannedWalrusFood.extractContents());
+        assertTrue(gary.hasEaten(walrusFood));
+    }
+
+    @Test
+    public void testWalrusEatingNonWalrusFood() {
+        Walrus gary = new Walrus();
+        WalrusFood walrusFood = new WalrusFood();
+
+        FeedsWalrus feedsWalrus = new FeedsWalrus();
+        assertThrows(ClassCastException.class, () -> feedsWalrus.feed(gary, (CannedWalrusFood) new Object())) ;
     }
 }
